d417b87c41df23e3600876895e0363eb0c5a70c2
Osvaldo <42978404+Quiroptero@users.noreply.github.com>
2022-06-26 06:50:58 -0500

    Add custom zsh aliases

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,4 @@
+# meta-aliases
+alias aliases='atom ~/.oh-my-zsh/custom/aliases.zsh'
+alias saliases='source ~/.oh-my-zsh/custom/aliases.zsh'
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
