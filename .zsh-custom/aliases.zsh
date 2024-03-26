b50c19b7398ec250a550a188aa2369fbd10d99d7
Osvaldo <42978404+Quiroptero@users.noreply.github.com>
2024-03-26 12:57:30 -0600

    Add Zsh and Oh My Zsh! config files

---
+++ .zsh-custom/aliases.zsh
@@ -0,0 +1,13 @@
+# meta-aliases
+alias aliases='nvim ~/.zsh-custom/aliases.zsh'
+alias saliases='source ~/.zsh-custom/aliases.zsh'
+
+# exiftool
+alias exifs="exiftool -T -filename -datetimeoriginal -focallength -fnumber -iso -shutterspeed"
+
+# dotfiles
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# utils
+alias pbcopy='xclip -selection clipboard'
+
