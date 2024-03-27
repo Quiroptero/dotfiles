bbe6819045292ac9b79b2f661337e599af16459a
Osvaldo <42978404+Quiroptero@users.noreply.github.com>
2024-03-27 09:43:20 -0600

    Add general git aliases for log and status. Replace long-named alias 'dotfiles' with a shorter 'dot'. Use double quotes in the alias definition consistently

--- .zsh-custom/aliases.zsh
+++ .zsh-custom/aliases.zsh
@@ -1,13 +1,18 @@
 # meta-aliases
-alias aliases='nvim ~/.zsh-custom/aliases.zsh'
-alias saliases='source ~/.zsh-custom/aliases.zsh'
+alias aliases="nvim ~/.zsh-custom/aliases.zsh"
+alias saliases="source ~/.zsh-custom/aliases.zsh"
 
 # exiftool
 alias exifs="exiftool -T -filename -datetimeoriginal -focallength -fnumber -iso -shutterspeed"
 
 # dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias ds="dot status -sb"
 
 # utils
-alias pbcopy='xclip -selection clipboard'
+alias pbcopy="xclip -selection clipboard"
+
+# git
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gs="git status -sb"
 
