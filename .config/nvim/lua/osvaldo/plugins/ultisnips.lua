f9331fc676009c6adb04f0b361c20e06b9bd088c
Osvaldo <42978404+Quiroptero@users.noreply.github.com>
2024-04-04 22:23:47 -0600

    Delete plugin SirVer/ultisnips from the configuration.
    I used the ultisnips plugin for its capabilities to define
    markdown-specific snippets. However, I recently found other plugins
    (kylechui/nvim-surround and windwp/nvim-autopairs) that replace the need
    of ultisnips and offer more features.

--- .config/nvim/lua/osvaldo/plugins/ultisnips.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "SirVer/ultisnips",
-  config = function()
-    vim.g.UltiSnipsSnippetDirectories = { "custom_snippets" }
-    vim.g.UltiSnipsExpandTrigger = "<tab>"
-    vim.g.UltiSnipsJumpForwardTrigger = "<c-j>"
-    vim.g.UltiSnipsJumpBackwardTrigger = "<c-k>"
-  end,
-}
