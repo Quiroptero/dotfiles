471fd5498e331fde9aecc647bf1513cb3c7bd3ce
Osvaldo <42978404+Quiroptero@users.noreply.github.com>
2024-04-04 22:35:18 -0600

    Import the LSP directory in Lazy. Remove the colorscheme from Lazy, it will be handled in a dedicated plugin file

--- .config/nvim/lua/osvaldo/lazy.lua
+++ .config/nvim/lua/osvaldo/lazy.lua
@@ -5,21 +5,15 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "osvaldo.plugins" } }, {
-  install = {
-    colorscheme = { "nightfly" },
-  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
+require("lazy").setup({ { import = "osvaldo.plugins" }, { import = "osvaldo.plugins.lsp" } }, {
   change_detection = {
     notify = false,
   },
 })
+
