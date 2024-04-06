4c437106cfd7c93a5489178344e863087f9d2f6d
Osvaldo <42978404+Quiroptero@users.noreply.github.com>
2024-04-06 15:16:58 -0600

    I prefer 4 spaces for indentation purposes

--- .config/nvim/lua/osvaldo/core/options.lua
+++ .config/nvim/lua/osvaldo/core/options.lua
@@ -6,23 +6,23 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2       -- 2 spaces for tabs
-opt.shiftwidth = 2    -- 2 spaces for indent width
-opt.expandtab = true  -- expand tab to spaces
+opt.tabstop = 4 -- 2 spaces for tabs
+opt.shiftwidth = 4 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 opt.cursorline = true
 
 -- visual settings
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
