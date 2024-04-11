f1a17a6075a79eae231d12cb5bc4f900abc6c7ec
Osvaldo Miranda <42978404+Quiroptero@users.noreply.github.com>
2024-04-11 00:43:02 -0600

    Place the custom 'surrounds' in the main `setup` function instead of in `buffer_setup`. The later was causing that the custom surrounds worked on one buffer and one buffer only, while the former makes the surrounds available everywhere. Also, reduce the number of lines

--- .config/nvim/lua/osvaldo/plugins/surround.lua
+++ .config/nvim/lua/osvaldo/plugins/surround.lua
@@ -1,21 +1,18 @@
 return {
 	"kylechui/nvim-surround",
-	event = { "BufReadPre", "BufNewFile" },
+    event = { "BufReadPre", "BufNewFile" },
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
 	config = function()
 		require("nvim-surround").setup({
-			-- Configuration here, or leave empty to use defaults
-		})
-		-- https://github.com/kylechui/nvim-surround/discussions/53#discussioncomment-3134891
-		require("nvim-surround").buffer_setup({
+			-- According to the documentation, the setup function is strictly additive,
+            -- so it's safe to add new "surroundings" to the table.
+            -- See: `:h nvim-surround.configuration`.
 			surrounds = {
+                -- https://github.com/kylechui/nvim-surround/discussions/53#discussioncomment-3134891
 				["l"] = {
 					add = function()
 						local clipboard = vim.fn.getreg("+"):gsub("\n", "")
-						return {
-							{ "[" },
-							{ "](" .. clipboard .. ")" },
-						}
+						return { { "[" }, { "](" .. clipboard .. ")" } }
 					end,
 					find = "%b[]%b()",
 					delete = "^(%[)().-(%]%b())()$",
@@ -23,37 +20,15 @@ return {
 						target = "^()()%b[]%((.-)()%)$",
 						replacement = function()
 							local clipboard = vim.fn.getreg("+"):gsub("\n", "")
-							return {
-								{ "" },
-								{ clipboard },
-							}
+							return { { "" }, { clipboard } }
 						end,
 					},
 				},
-				["s"] = {
-					add = function()
-						return {
-							{ "«" },
-							{ "»" },
-						}
-					end,
-				},
-				["e"] = {
-					add = function()
-						return {
-							{ "¡" },
-							{ "!" },
-						}
-					end,
-				},
-				["q"] = {
-					add = function()
-						return {
-							{ "¿" },
-							{ "?" },
-						}
-					end,
-				},
+                -- Spanish punctuation marks
+                -- https://github.com/kylechui/nvim-surround/discussions/53#discussioncomment-9016771
+				["s"] = { add = function() return { { "«" }, { "»" } } end },
+				["e"] = { add = function() return { { "¡" }, { "!" } } end },
+				["q"] = { add = function() return { { "¿" }, { "?" } } end },
 			},
 		})
 	end,
