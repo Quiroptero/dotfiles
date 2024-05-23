7039775ade500b95ac907156645c1e4b444ed8e7
Osvaldo Miranda <services@omiranda.dev>
2024-05-23 15:23:15 -0600

    Use the configuration for Zsh, it was wrongly set for Bash

--- .zsh-custom/pyenv.zsh
+++ .zsh-custom/pyenv.zsh
@@ -1,4 +1,5 @@
 # https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
