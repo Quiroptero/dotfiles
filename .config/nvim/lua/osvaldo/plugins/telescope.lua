c3453f312996dc0957feb63a87eda8ebe757b5e8
Osvaldo Miranda <42978404+Quiroptero@users.noreply.github.com>
2024-06-02 15:58:09 -0600

    Add keymap to search a string in the current working directory with telescope without ignoring any file to avoid .gitignore

--- .config/nvim/lua/osvaldo/plugins/telescope.lua
+++ .config/nvim/lua/osvaldo/plugins/telescope.lua
@@ -32,6 +32,7 @@ return {
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fu", function() require("telescope.builtin").live_grep({ additional_args = { "-u" } }) end, { desc = "Find string in cwd, no ignore files" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
