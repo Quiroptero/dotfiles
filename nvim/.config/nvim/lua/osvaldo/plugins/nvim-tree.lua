3e6697bf3493fc6d59688a4bcf6cdb2d81439de6
Osvaldo Miranda <osvaldo+git@omiranda.dev>
2025-02-24 14:56:06 +0000

    Reorganize nvim-tree commands

--- nvim/.config/nvim/lua/osvaldo/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/osvaldo/plugins/nvim-tree.lua
@@ -34,9 +34,9 @@ return {
         -- set keymaps
         local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file [E]xplorer" })
+        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "[F]ind current file in the tree" })
+        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "[C]ollapse file explorer" })
+        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "[R]efresh file explorer" })
     end
 }
