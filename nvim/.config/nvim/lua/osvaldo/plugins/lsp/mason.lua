5739a502fa03405247462c35cdfffeafd85b2326
Osvaldo Miranda <git@osvaldomiranda.me>
2025-07-16 09:50:31 -0600

    refactor: Rework LSP strategy to achieve consistent behavior, autocompletion and diagnostics

--- nvim/.config/nvim/lua/osvaldo/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/osvaldo/plugins/lsp/mason.lua
@@ -1,39 +1,14 @@
+-- mason is a package manager for LSP servers
+-- NOTE: requires Node!
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
-        local mason_tool_installer = require("mason-tool-installer")
-
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
-        mason_lspconfig.setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "html",
-                "cssls",
-                "lua_ls",
-            },
-        })
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "prettier", -- prettier formatter
-                "stylua", -- lua formatter
-                "isort", -- python formatter
-                "black", -- python formatter
-                "pylint",
-            },
-        })
-    end,
+    "mason-org/mason.nvim",
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✔",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+            }
+        }
+    }
 }
--- nvim/.config/nvim/lua/osvaldo/plugins/lsp/nvim-lsp-file-operations.lua
+++ nvim/.config/nvim/lua/osvaldo/plugins/lsp/nvim-lsp-file-operations.lua
@@ -1,12 +1,10 @@
 return {
-  {
     "antosha417/nvim-lsp-file-operations",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-tree.lua",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-tree.lua",
     },
     config = function()
-      require("lsp-file-operations").setup()
+        require("lsp-file-operations").setup()
     end,
-  },
 }
