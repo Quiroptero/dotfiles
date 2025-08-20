ac8263f80fc145271159fd9f0718c84fb18db5a0
Osvaldo Miranda <git@osvaldomiranda.me>
2025-08-19 20:57:44 -0600

    feat: try to force treesitter to load quickly

--- nvim/.config/nvim/lua/osvaldo/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/osvaldo/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
+  lazy = false,
+  event = { "VimEnter" },
   build = ":TSUpdate",
   dependencies = { "windwp/nvim-ts-autotag" },
   config = function()
